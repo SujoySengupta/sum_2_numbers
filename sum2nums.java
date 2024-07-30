89c3c45e450ccd8fbeca5ea9f22969587c9dc849
Sujoy Sengupta <145558068+SujoySengupta@users.noreply.github.com>
2024-07-30 18:26:16 +0530

    Add files via upload

---
+++ sum2nums.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sum2nums {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        a=inp.nextInt();
+        b=inp.nextInt();
+        System.out.println("The sum of the two numbers is: "+(a+b));
+    }
+    
+}
